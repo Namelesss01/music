a2787f3ad3f40967992d4bbcc371e53fe78dd30d
Namelesss01 <zajloobekovali00@gmail.com>
2024-11-16 00:36:46 +0600

    5

--- src/shared/authorization/Auth.tsx
+++ src/shared/authorization/Auth.tsx
@@ -1,6 +1,5 @@
 import { Button } from "../../components/ui/button";
 import { Card, CardContent, CardFooter } from "../../components/ui/card";
-import { Checkbox } from "../../components/ui/checkbox";
 import {
   Dialog,
   DialogTrigger,
@@ -8,7 +7,6 @@ import {
   DialogTitle,
 } from "../../components/ui/dialog";
 import { Input } from "../../components/ui/input";
-import { Label } from "../../components/ui/label";
 import {
   Tabs,
   TabsContent,
